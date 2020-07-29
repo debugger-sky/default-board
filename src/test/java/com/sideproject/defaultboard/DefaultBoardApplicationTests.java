9efa044cb082344115c42d4817ccd476dd0ff230
SKY <berno579@gmail.com>
2020-07-30 08:37:02 +0900

    Initial commit

---
+++ src/test/java/com/sideproject/defaultboard/DefaultBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sideproject.defaultboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DefaultBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
